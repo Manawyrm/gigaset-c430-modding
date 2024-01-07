63fe0e4aa6ea970f650fa8aef7130dd20dc7e7ee
Manawyrm <git@tbspace.de>
2024-01-07 19:54:16 +0100

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -10,38 +10,38 @@
 # string names of the ringtones and then replace the values accordingly.
 
 # [midi-export]$ ls -lA *.mid
-# -rw-r--r-- 1 tobias users  123 21.11.2022 17:15 extract-1662195.mid
-# -rw-r--r-- 1 tobias users   34 21.11.2022 17:15 extract-1662318.mid
-# -rw-r--r-- 1 tobias users  110 21.11.2022 17:15 extract-1662352.mid
-# -rw-r--r-- 1 tobias users   48 21.11.2022 17:15 extract-1662462.mid
-# -rw-r--r-- 1 tobias users  403 21.11.2022 17:15 extract-1662510.mid
-# -rw-r--r-- 1 tobias users   72 21.11.2022 17:15 extract-1662913.mid
-# -rw-r--r-- 1 tobias users   78 21.11.2022 17:15 extract-1662985.mid
-# -rw-r--r-- 1 tobias users  207 21.11.2022 17:15 extract-1663063.mid
-# -rw-r--r-- 1 tobias users  125 21.11.2022 17:15 extract-1663270.mid
-# -rw-r--r-- 1 tobias users  503 21.11.2022 17:15 extract-1663395.mid
-# -rw-r--r-- 1 tobias users  155 21.11.2022 17:15 extract-1663898.mid
-# -rw-r--r-- 1 tobias users  369 21.11.2022 17:15 extract-1664053.mid
-# -rw-r--r-- 1 tobias users  535 21.11.2022 17:15 extract-1664422.mid
-# -rw-r--r-- 1 tobias users   84 21.11.2022 17:15 extract-1664957.mid
-# -rw-r--r-- 1 tobias users  252 21.11.2022 17:15 extract-1665041.mid
-# -rw-r--r-- 1 tobias users  707 21.11.2022 17:15 extract-1665293.mid
-# -rw-r--r-- 1 tobias users  137 21.11.2022 17:15 extract-1666000.mid
-# -rw-r--r-- 1 tobias users  728 21.11.2022 17:15 extract-1666137.mid
-# -rw-r--r-- 1 tobias users 1901 21.11.2022 17:15 extract-1666865.mid - Expect -  Cruel Angel Thesis
-# -rw-r--r-- 1 tobias users 3413 21.11.2022 17:15 extract-1668766.mid - Fact - Hello OMFG
-# -rw-r--r-- 1 tobias users 1449 21.11.2022 17:15 extract-1672179.mid
-# -rw-r--r-- 1 tobias users 1990 21.11.2022 17:15 extract-1673628.mid - Good news - Still alive
-# -rw-r--r-- 1 tobias users 4026 21.11.2022 17:15 extract-1675618.mid - Sugar - 1000 weisse Lilien
-# -rw-r--r-- 1 tobias users 2223 21.11.2022 17:15 extract-1679644.mid
-# -rw-r--r-- 1 tobias users 2831 21.11.2022 17:15 extract-1681867.mid - Fadin - Super 8
-# -rw-r--r-- 1 tobias users 4701 21.11.2022 17:15 extract-1684698.mid - Fusion - 23
-# -rw-r--r-- 1 tobias users 3792 21.11.2022 17:15 extract-1689399.mid - Trippin - Megaman
-# -rw-r--r-- 1 tobias users 3773 21.11.2022 17:15 extract-1693191.mid - Smile - Nyan
-# -rw-r--r-- 1 tobias users 3229 21.11.2022 17:15 extract-1696964.mid - Flow - Never Gonna
-# -rw-r--r-- 1 tobias users 6474 21.11.2022 17:15 extract-1700193.mid - Tiger - C64
-# -rw-r--r-- 1 tobias users  161 21.11.2022 17:15 extract-1706667.mid
-# -rw-r--r-- 1 tobias users  119 21.11.2022 17:15 extract-1706828.mid
+# -rw-r--r-- 1 manawyrm users  123 21.11.2022 17:15 extract-1662195.mid
+# -rw-r--r-- 1 manawyrm users   34 21.11.2022 17:15 extract-1662318.mid
+# -rw-r--r-- 1 manawyrm users  110 21.11.2022 17:15 extract-1662352.mid
+# -rw-r--r-- 1 manawyrm users   48 21.11.2022 17:15 extract-1662462.mid
+# -rw-r--r-- 1 manawyrm users  403 21.11.2022 17:15 extract-1662510.mid
+# -rw-r--r-- 1 manawyrm users   72 21.11.2022 17:15 extract-1662913.mid
+# -rw-r--r-- 1 manawyrm users   78 21.11.2022 17:15 extract-1662985.mid
+# -rw-r--r-- 1 manawyrm users  207 21.11.2022 17:15 extract-1663063.mid
+# -rw-r--r-- 1 manawyrm users  125 21.11.2022 17:15 extract-1663270.mid
+# -rw-r--r-- 1 manawyrm users  503 21.11.2022 17:15 extract-1663395.mid
+# -rw-r--r-- 1 manawyrm users  155 21.11.2022 17:15 extract-1663898.mid
+# -rw-r--r-- 1 manawyrm users  369 21.11.2022 17:15 extract-1664053.mid
+# -rw-r--r-- 1 manawyrm users  535 21.11.2022 17:15 extract-1664422.mid
+# -rw-r--r-- 1 manawyrm users   84 21.11.2022 17:15 extract-1664957.mid
+# -rw-r--r-- 1 manawyrm users  252 21.11.2022 17:15 extract-1665041.mid
+# -rw-r--r-- 1 manawyrm users  707 21.11.2022 17:15 extract-1665293.mid
+# -rw-r--r-- 1 manawyrm users  137 21.11.2022 17:15 extract-1666000.mid
+# -rw-r--r-- 1 manawyrm users  728 21.11.2022 17:15 extract-1666137.mid
+# -rw-r--r-- 1 manawyrm users 1901 21.11.2022 17:15 extract-1666865.mid - Expect -  Cruel Angel Thesis
+# -rw-r--r-- 1 manawyrm users 3413 21.11.2022 17:15 extract-1668766.mid - Fact - Hello OMFG
+# -rw-r--r-- 1 manawyrm users 1449 21.11.2022 17:15 extract-1672179.mid
+# -rw-r--r-- 1 manawyrm users 1990 21.11.2022 17:15 extract-1673628.mid - Good news - Still alive
+# -rw-r--r-- 1 manawyrm users 4026 21.11.2022 17:15 extract-1675618.mid - Sugar - 1000 weisse Lilien
+# -rw-r--r-- 1 manawyrm users 2223 21.11.2022 17:15 extract-1679644.mid
+# -rw-r--r-- 1 manawyrm users 2831 21.11.2022 17:15 extract-1681867.mid - Fadin - Super 8
+# -rw-r--r-- 1 manawyrm users 4701 21.11.2022 17:15 extract-1684698.mid - Fusion - 23
+# -rw-r--r-- 1 manawyrm users 3792 21.11.2022 17:15 extract-1689399.mid - Trippin - Megaman
+# -rw-r--r-- 1 manawyrm users 3773 21.11.2022 17:15 extract-1693191.mid - Smile - Nyan
+# -rw-r--r-- 1 manawyrm users 3229 21.11.2022 17:15 extract-1696964.mid - Flow - Never Gonna
+# -rw-r--r-- 1 manawyrm users 6474 21.11.2022 17:15 extract-1700193.mid - Tiger - C64
+# -rw-r--r-- 1 manawyrm users  161 21.11.2022 17:15 extract-1706667.mid
+# -rw-r--r-- 1 manawyrm users  119 21.11.2022 17:15 extract-1706828.mid
 
 dd if=gigaset_c430hx.bin of=gigaset_c430hx_mod.bin bs=8k
 
